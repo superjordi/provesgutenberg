4388e3ce43154af62f3f6ea2ae111a4d376d3e80
jmarti <38979681+superjordi@users.noreply.github.com>
2019-04-29 12:30:57 +0200

    Start index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <META NAME="robots" CONTENT="noindex,nofollow">
+    <title>Proves Gutenberg</title>
+    <style>
+        h1 {
+            position: fixed;
+            top: 50%;
+            left: 0;
+            right: 0;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>provesgutenberg.fundesplai.org està lliure de contingut i a punt per fer servir</h1>
+</body>
+</html>
